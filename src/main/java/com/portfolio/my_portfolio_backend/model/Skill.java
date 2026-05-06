149df252d1c3fb3c1cd168785011e2a72dca2dfc
Alan Álvarez <44711315+AlanRAN7@users.noreply.github.com>
2026-05-06 00:04:47 -0600

    Se encuentra con sus validaciones en el modelo

--- src/main/java/com/portfolio/my_portfolio_backend/model/Skill.java
+++ src/main/java/com/portfolio/my_portfolio_backend/model/Skill.java
@@ -1,5 +1,9 @@
 package com.portfolio.my_portfolio_backend.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Skill {
 private Long id;
+
+@NotBlank(message =  "El nombre no puede estar vacío")
 private String name;
+
+@NotNull(message = "El porcentaje no debe de ser nulo")
+@Min(value = 0, message = "El minimo valor debe de ser 0")
+@Max(value=100, message = "El máximo valor debe de ser 100")
 private Integer levelPercentage;
+@NotBlank(message = "La clase del icono no puede estar vacía")
 private String iconClass;
+@NotBlank(message = "La skill debe de estar sujeto con una persona")
 private Long personalInfoId;
 }
