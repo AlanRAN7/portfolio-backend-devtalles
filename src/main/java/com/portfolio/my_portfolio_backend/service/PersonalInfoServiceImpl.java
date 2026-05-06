4681b62a6ab5e4d5418cf9eeb3d8354ad72db107
Alan Álvarez <44711315+AlanRAN7@users.noreply.github.com>
2026-05-05 23:55:24 -0600

    Transactional is added

--- src/main/java/com/portfolio/my_portfolio_backend/service/PersonalInfoServiceImpl.java
+++ src/main/java/com/portfolio/my_portfolio_backend/service/PersonalInfoServiceImpl.java
@@ -4,6 +4,7 @@ import com.portfolio.my_portfolio_backend.exception.ValidationException;
 import com.portfolio.my_portfolio_backend.model.PersonalInfo;
 import com.portfolio.my_portfolio_backend.repository.IPersonalInfoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 
@@ -23,6 +24,7 @@ public class PersonalInfoServiceImpl implements IPersonalInfoService {
     }
 
     @Override
+    @Transactional // -> Ya con esto se le dice a Springboot que se debe de usar como una unidad atómica [y en la capa de servicio]
     public PersonalInfo save(PersonalInfo personalInfo) {
         BindingResult result = new BeanPropertyBindingResult(personalInfo, "personalInfo");
         validator.validate(personalInfo, result);
@@ -33,16 +35,19 @@ public class PersonalInfoServiceImpl implements IPersonalInfoService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<PersonalInfo> findById(Long id) {
         return personalInfoRepository.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<PersonalInfo> findAll() {
         return personalInfoRepository.findAll();
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         personalInfoRepository.deleteById(id);
 
